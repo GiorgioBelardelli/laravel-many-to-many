6f27c6465cd6ebfa5dec4825bd3e6ccd451424e4
GiorgioBelardelli <asterapocalypse@gmail.com>
2024-02-22 19:13:54 +0100

    ProjectTableSeeder + first project seed

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
+use App\Models\Type;
 
 class ProjectTableSeeder extends Seeder
 {
@@ -15,6 +16,15 @@ class ProjectTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Project :: factory() 
+        -> count(30) 
+        -> make() 
+        -> each(function($project){
+            
+            $type = Type :: inRandomOrder() -> first();
+            $project -> type() -> associate($type);
+
+            $project -> save();
+        });
     }
 }
